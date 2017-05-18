fa7bba802d552d781b7803e85b1d6f4f02001fcb
arao519 <arao519@PR32.hhs.win.fuhsd.org>
2017-05-18 09:47:09 -0700

    ugly but it's there

--- StrategyGame/src/map/Map.java
+++ StrategyGame/src/map/Map.java
@@ -111,13 +111,15 @@ public class Map {
 			g.drawLine(xCor, 0, xCor, height);
 			g.drawLine(0, yCor, width, yCor);
 			for(int y = 0; y < 20; y++) {
-				if (map[x][y].getUnit() != null) {
+				if (map[x][y].getTerrain() == "land");
+					map[x][y].draw(g, x*xDist, yDist, y*yDist, yDist);
+				if (map[x][y].getUnit() != null)
 					map[x][y].getUnit().draw(g, x*xDist + xDist/4, xDist/2, y*yDist + yDist/4, yDist/2);
-				}
+				
 			}
 			xCor += xDist;
 			yCor += yDist;
 		}
 	}
-
+		
 }
